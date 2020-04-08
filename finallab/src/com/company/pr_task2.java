faaa85c32502e7acd5a8a263957054e17fb70af8
Jaffar Abbas <gamaportal8@gmail.com>
2020-04-09 00:49:44 +0500

    ...

--- finallab/src/com/company/pr_task2.java
+++ finallab/src/com/company/pr_task2.java
@@ -32,12 +32,13 @@ class Toaday extends Appointment {
     Scanner obj = new Scanner(System.in);
 
 
-    void toaday() {
-        // date = obj.nextInt();
+    void today() {
+         System.out.println("Day : will 1");
+         date = obj.nextInt();
         for (int j = 1; j <= 3; j++) {
             arr[1] = arr("jaffar", 1, 3, 2020);
-            arr[2] = arr("fahad", 2, 3, 2020);
-            arr[3] = arr("ahmed", 3, 3, 2020);
+            arr[2] = arr("fahad", 1, 3, 2020);
+            arr[3] = arr("ahmed", 1, 3, 2020);
         }
 
         for (int i = 1; i <= 3; i++) {
@@ -52,6 +53,7 @@ class Toaday extends Appointment {
     void add_Apoitment_Today(){
         System.out.println("how many apoitment will this day have  : ");
         n = obj.nextInt();
+        System.out.println("Day : ");
         date = obj.nextInt();
         for(int i=1;i<=n;i++){
             arr[date]=add_appoitment();
@@ -68,7 +70,7 @@ class Toaday extends Appointment {
 
 class Monthly extends Appointment {
        
-    void mont_check(){
+    void month(){
         System.out.println("Day : ");
         date = obj.nextInt();
         for(int i=0;i<=10;i++){
@@ -104,13 +106,112 @@ class Monthly extends Appointment {
     }
 }
 
+class year extends Appointment{
+    void year_(){
+        System.out.println("Day : ");
+        date = obj.nextInt();
+        for(int i=0;i<=10;i++){
+            arr[1]=arr("des", 1, 2, 2020);
+            arr[2]=arr("des", 2, 2, 2020);
+            arr[3]=arr("des", 3, 2, 2020);
+        }
+
+        arr[4]=add_appoitment();
+        for (int i = 1; i <= 4; i++) {
+            if (i == date) {
+                System.out.println(arr[i]);
+                break;
+            }
+        }
+    }
+
+    void add_Apoitment_year(){
+        System.out.println("how many apoitment will this month have  : ");
+        n = obj.nextInt();
+        System.out.println("Day : ");
+        date = obj.nextInt();
+        for(int i=1;i<=n;i++){
+            arr[date]=add_appoitment();
+        }
+
+        for (int i = 1; i <= date; i++) {
+            if (i == date) {
+                System.out.println(arr[i]);
+                break;
+            }
+        }
+    }
+}
+
+class testclass_{
+    Scanner scn = new Scanner(System.in);
+
+    Toaday obj = new Toaday();
+    Monthly obj2 = new Monthly();
+    year obj3 = new year();
+
+
+    void select(){
+        System.out.println("Enter tour desire  \n T For Today \n M For Month\n Y For Year");
+        char a =Character.toUpperCase(scn.next().charAt(0));
+
+        switch (a){
+            case 'T':
+                obj.today();
+                break;
+            case 'M':
+                obj2.month();
+                break;
+            case 'Y':
+                obj3.year_();
+            break;
+            default:
+                System.out.println("Error");
+                break;
+        }
+    }
+
+    void add_app_select(){
+        System.out.println("In which class you want to add \n T For Today \n M For Month\n Y For Year");
+        char a =Character.toUpperCase(scn.next().charAt(0));
+
+        switch (a){
+            case 'T':
+                obj.add_Apoitment_Today();
+                break;
+            case 'M':
+                obj2.add_Apoitment_month();
+                break;
+            case 'Y':
+                obj3.add_Apoitment_year();
+                break;
+            default:
+                System.out.println("Error");
+                break;
+        }
+    }
+
+    void Final_select(){
+        System.out.println("What to want to do (See or Add)");
+        char a =Character.toUpperCase(scn.next().charAt(0));
+
+        switch (a){
+            case 'S':
+                select();
+                break;
+            case 'A':
+               add_app_select();
+                break;
+            default:
+                System.out.println("Error");
+             break;
+        }
+    }
+}
+
 public class pr_task2 {
     public static void main( String[] args) {
-        Toaday obj = new Toaday();
-        Monthly obj2 = new Monthly();
-        obj.add_Apoitment_Today();
-        // obj.toaday();
-//        obj2.mont_check();
-       // obj2.add_met();
+        testclass_ obj = new testclass_();
+        obj.Final_select();
     }
 }
